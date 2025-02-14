59c4f6b350c6bd336c499a14584e96357dd44b50
Aron Jones <aron.jones@gmail.com>
2025-02-14 17:24:15 -0500

    don't port neuroevo

--- src/app/experiments/neuroevolution/path.ts
+++
@@ -1,3 +0,0 @@
-export function neuroevolutionPath() {
-  return '/experiments/neuroevolution'
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { neuroevolutionPath } from '@/app/experiments/neuroevolution/path'
 import { postDetailsPath } from '@/app/posts/[slug]/path'
 import { Inline } from '@/components/inline'
+import { PageContainer } from '@/components/page-container'
 import { Stack } from '@/components/stack'
 import { TextLink } from '@/components/text-link'
 import { Text } from '@/components/text/text'
@@ -9,7 +9,7 @@ import { postList } from '@/lib/post-list'
 const experiments = [
   {
     title: 'Neuroevolution',
-    href: neuroevolutionPath(),
+    href: 'https://neuroevolution.defrex.com',
     date: new Date('2025-02-14'),
     type: 'experiment',
   },
@@ -24,25 +24,23 @@ export default function Page() {
   const items = [...posts, ...experiments].sort((a, b) => b.date.getTime() - a.date.getTime())
 
   return (
-    <div className="container mx-auto max-w-[1024px]">
-      <Stack gap={8}>
-        {items.map((item, index) => (
-          <TextLink
-            key={index}
-            href={'href' in item ? item.href : postDetailsPath(item.slug)}
-            className="group block"
-          >
-            <Stack gap={3}>
-              <Text value={item.title} size="lg" color="inherit" />
-              <Inline gap={1}>
-                <Text value={item.date.toLocaleDateString()} size="sm" color="light" />
-                <Text value="·" size="sm" color="light" />
-                <Text value={item.type} size="sm" color="light" />
-              </Inline>
-            </Stack>
-          </TextLink>
-        ))}
-      </Stack>
-    </div>
+    <PageContainer>
+      {items.map((item, index) => (
+        <TextLink
+          key={index}
+          href={'href' in item ? item.href : postDetailsPath(item.slug)}
+          className="group block"
+        >
+          <Stack gap={3}>
+            <Text value={item.title} size="lg" color="inherit" />
+            <Inline gap={1}>
+              <Text value={item.date.toLocaleDateString()} size="sm" color="light" />
+              <Text value="·" size="sm" color="light" />
+              <Text value={item.type} size="sm" color="light" />
+            </Inline>
+          </Stack>
+        </TextLink>
+      ))}
+    </PageContainer>
   )
 }
