2276d93eddd939c7b954ccc18bef8977ac3d8048
Aron Jones <aron.jones@gmail.com>
2025-03-26 16:59:10 -0400

    add spendwell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,22 @@ import { ExternalLinkIcon } from 'lucide-react'
 
 const experiments = [
   {
+    title: 'Spendwell',
+    href: 'https://spendwell.defrex.com/',
+    date: new Date('2025-03-26'),
+    type: 'Tool',
+  },
+  {
     title: 'Dicetribution',
     href: 'https://dicestribution.com/',
     date: new Date('2024-11-23'),
-    type: 'tool',
+    type: 'Tool',
   },
   {
     title: 'Neuroevolution',
     href: 'https://neuroevolution.defrex.com/',
     date: new Date('2022-12-01'),
-    type: 'experiment',
+    type: 'Experiment',
   },
 ]
 
@@ -29,7 +35,7 @@ export const metadata = {
 export default function Page() {
   const posts = postList().map((post) => ({
     ...post,
-    type: 'post',
+    type: 'Article',
   }))
 
   const items = [...posts, ...experiments].sort((a, b) => b.date.getTime() - a.date.getTime())
@@ -39,6 +45,9 @@ export default function Page() {
       {items.map((item, index) => (
         <TextLink key={index} href={'href' in item ? item.href : postDetailsPath(item.slug)}>
           <Stack gap={3}>
+            {/* <div className="border border-neutral-400 rounded py-px px-1">
+            </div> */}
+            <Text value={item.type} size="sm" color="light" />
             <Inline gap={1}>
               <Text value={item.title} color="inherit" />
               {'href' in item && !item.href.startsWith('/') ? (
@@ -47,8 +56,7 @@ export default function Page() {
             </Inline>
             <Inline gap={1}>
               <Text value={item.date.toLocaleDateString()} size="sm" color="light" />
-              <Text value="·" size="sm" color="light" />
-              <Text value={item.type} size="sm" color="light" />
+              {/* <Text value="·" size="sm" color="light" /> */}
             </Inline>
           </Stack>
         </TextLink>
